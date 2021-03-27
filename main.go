15e141025edd2ce0b2d1d0dac45299aa55c93e13
alushkja <aldo.lushkja@consulenti.fastweb.it>
2021-03-27 01:28:21 +0100

    serving html templates

--- main.go
+++ main.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-	"errors"
 	"fmt"
+	"html/template"
 	"net/http"
 )
 
@@ -10,44 +10,26 @@ const portNumber = ":8080"
 
 // Home is the home page handler
 func Home(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "This is the home page")
+	renderTemplate(w, "home.page.tmpl")
 }
 
 // About is the about page handler
 func About(w http.ResponseWriter, r *http.Request) {
-	sum := addValues(2, 3)
-	_, _ = fmt.Fprint(w, fmt.Sprintf("This is the about page and 2 + 3 is %d", sum))
+	renderTemplate(w, "about.page.tmpl")
 }
 
-// Divide is the divide page handler
-func Divide(w http.ResponseWriter, r *http.Request) {
-	f, err := divideValues(100, 0)
-	if err != nil {
-		fmt.Fprint(w, fmt.Sprintf("Cannot divide by 0"))
-		return
+func renderTemplate(w http.ResponseWriter, tmpl string) {
+	parsedTemplate, _ := template.ParseFiles("./templates/" + tmpl)
+	err := parsedTemplate.Execute(w, nil)
+	if err != nil{
+		fmt.Println("error parsing template: ", err)
 	}
-	fmt.Fprint(w, fmt.Sprintf("%f divided by %f is %f",  100.0, 0, f))
-}
-
-//addValues adds two integers and return the sum
-func addValues(x, y int) int {
-	return x + y
-}
-
-func divideValues(x, y float32) (float32, error) {
-	if y <= 0 {
-		err := errors.New("Cannot divide by zero")
-		return 0, err
-	}
-	result := x / y
-	return result, nil
 }
 
 //main this is the main application
 func main() {
 	http.HandleFunc("/", Home)
 	http.HandleFunc("/about", About)
-	http.HandleFunc("/divide", Divide)
 
 	fmt.Println(fmt.Sprintf("Starting application on port %s", portNumber))
 
