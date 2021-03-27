bfc8d56d23ae71a1e000ea6f40f325e25a8a7417
alushkja <aldo.lushkja@consulenti.fastweb.it>
2021-03-27 18:07:26 +0100

    setting application wide configuration

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"fmt"
+	"github.com/aldolushkja/gohello-web/config"
 	"github.com/aldolushkja/gohello-web/pkg/handlers"
+	"github.com/aldolushkja/gohello-web/pkg/render"
+	"log"
 	"net/http"
 )
 
@@ -10,6 +13,13 @@ const portNumber = ":8080"
 
 //main this is the main application
 func main() {
+	var app config.AppConfig
+
+	tc, err := render.CreateTemplateCache()
+	if err != nil {
+		log.Fatal("Cannot create template cache", err)
+	}
+	app.TemplateCache = tc
 	http.HandleFunc("/", handlers.Home)
 	http.HandleFunc("/about", handlers.About)
 
