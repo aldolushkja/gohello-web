ffc982814eb648cfbfbe24f62f10fe82c8d32e7e
alushkja <aldo.lushkja@consulenti.fastweb.it>
2021-03-27 17:12:44 +0100

    enabling go modules and refactoring our code to use packages

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"github.com/aldolushkja/gohello-web/pkg/handlers"
 	"net/http"
 )
 
@@ -9,8 +10,8 @@ const portNumber = ":8080"
 
 //main this is the main application
 func main() {
-	http.HandleFunc("/", Home)
-	http.HandleFunc("/about", About)
+	http.HandleFunc("/", handlers.Home)
+	http.HandleFunc("/about", handlers.About)
 
 	fmt.Println(fmt.Sprintf("Starting application on port %s", portNumber))
 
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/aldolushkja/gohello-web
+
+go 1.16
